f35b4e879fddd378a97442993559b111b74ced20
Sergey Melekhin <sergey@melekhin.me>
2026-09-13 11:02:14 +0700

    test(e2e): use pelican JPG image in screenshot test and remove unused pelican SVG

--- e2e/screenshots_test.go
+++ e2e/screenshots_test.go
@@ -147,7 +147,7 @@ func TestScreenshots(t *testing.T) {
 		time.Sleep(300 * time.Millisecond)
 
 		if u == "ivan" {
-			err = page.Locator("#file-input").SetInputFiles([]string{"../static/besedka.png"})
+			err = page.Locator("#file-input").SetInputFiles([]string{"../static/pelican.jpg"})
 			require.NoError(t, err)
 			err = page.Locator(".attach-indicator").WaitFor(playwright.LocatorWaitForOptions{
 				State: playwright.WaitForSelectorStateVisible,
